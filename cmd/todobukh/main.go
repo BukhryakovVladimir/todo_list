c4eb160a7ca62f9db0246d96c3a24e69bd0219dc
Vladimir Bukhryakov <buhryakov15@mail.ru>
2024-01-25 13:28:27 +0300

    :wrench: port, tsl certificate path and tsl key path are fetched from environmental variables.

--- cmd/todobukh/main.go
+++ cmd/todobukh/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 
@@ -18,11 +20,17 @@ func main() {
 
 	todobukhhandler.SetupRoutes(r)
 
-	certFile := "/etc/golang/ssl/localhost.crt" // Следует брать название сертификата из переменных среды (getenv)
-	keyFile := "/etc/golang/ssl/localhost.key"  // Следует брать название ключа из переменных среды (getenv)
+	//TSL_CERT=/etc/golang/ssl/localhost.crt # Полный путь к сертификату
+	//TSL_KEY=/etc/golang/ssl/localhost.key # Полный путь к ключу
+	//PORT=3000 # Только порт (цеоле число)
+
+	certFile := os.Getenv("TSL_CERT") // Следует брать название сертификата из переменных среды (getenv)
+	keyFile := os.Getenv("TSL_KEY")   // Следует брать название ключа из переменных среды (getenv)
+
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
 
 	// Use ListenAndServeTLS to start the HTTPS server
-	err := http.ListenAndServeTLS(":3000", certFile, keyFile, r)
+	err := http.ListenAndServeTLS(port, certFile, keyFile, r)
 	if err != nil {
 		// Handle error
 		panic(err)
